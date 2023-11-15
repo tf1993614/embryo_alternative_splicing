6cf4f22f14939fa3fb5e3ca43c2a16e827f38271
Feng Tang <763615402@qq.com>
2023-11-15 10:56:49 +0800

    change the transcriptome path since using different host

--- scripts/cellranger.sh
+++ scripts/cellranger.sh
@@ -4,9 +4,9 @@ set -e
 set -u
 set -o pipefail
 
-trans="/home/fengtang/project/embryo_splicing_site/data/reference_genome/refdata-gex-GRCh38-2020-A"
+trans="/data/embryo_alternative_splicing/human_data/human_transcriptome_index/refdata-gex-GRCh38-2020-A"
 file=$1
-
-name=$(basename ${file})
+ 
+name=$(basename -s ".fastq.gz" ${file})
 id="run_count_${name}"
 cellranger count --noexit --id=${id} --fastqs=${file} --sample=${name} --transcriptome=${trans}
