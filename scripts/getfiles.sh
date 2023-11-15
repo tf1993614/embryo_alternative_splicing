720f9e9e216946cdb634df934feb0fa7da62a696
Feng Tang <763615402@qq.com>
2023-11-15 10:55:53 +0800

    fix duplicate paths and suit for different path length

--- scripts/getfiles.sh
+++ scripts/getfiles.sh
@@ -1,13 +1,22 @@
 #！/bin/bash
 
+set -e
+set -u
+set -o pipefail
 
 paths=$(find "$2" -name "$1")
 paths=$(echo ${paths} | sort | uniq)
 
-echo $paths
+bash_array=()
+index=0
+for_number=1
 
 for path in ${paths}
 do
-	dir=$(echo ${path} | sed 's/\//\t/g' | awk -F "\t" '{print $1 "/" $2 "/" $3 "/" $4 "/" $5 "/" $6 "/" $7 "/" $8 "/"}')
-	echo ${dir}
+
+	dir=$(echo ${path} | sed 's/\//\t/g' | awk -F "\t" -v OFS="/" 'NF==3{print $1,$2};NF==4{print $1,$2,$3};NF==5{print $1,$2,$3,$4};NF==6{print $1,$2,$3,$4,$5};NF==7{print $1,$2,$3,$4,$5,$6};NF==8{print $1,$2,$3,$4,$5,$6,$7}')
+	bash_array[${index}]=${dir}
+	index=$(($index + $for_number))
 done
+
+echo ${bash_array[@]} | sed 's/ /\n/g'| sort | uniq
